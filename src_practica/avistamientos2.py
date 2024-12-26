6274ded28d16acdeb6c0bc90b62fe12d62b458bc
RDLCS <raudesan1@alum.us.es>
2024-12-27 00:58:23 +0100

    4.0

--- src_practica/avistamientos2.py
+++ src_practica/avistamientos2.py
@@ -1,7 +1,7 @@
 import csv
-from typing import NamedTuple,List,Dict,DefaultDict,Tuple,Optional
-from datetime import datetime
-from math import radians, sin, cos, asin, sqrt
+from typing import NamedTuple,List,Dict,DefaultDict,Tuple,Optional,Set
+from datetime import datetime,date,time,timedelta
+from math import radians, sin, cos, asin, sqrt,pi
 from collections import namedtuple, Counter, defaultdict
 import locale
 import statistics
@@ -9,7 +9,7 @@ import statistics
 coordenadas=NamedTuple('coordenadas',[('latitud',float),('longitud',float)])
 
 avistamiento=NamedTuple('avistamiento',[('fecha_hora',datetime),('ciudad',str),('estado',str),('forma',str),('duracion',int)
-                                        ,('comentarios',str),('ubicación',coordenadas)])
+                                        ,('comentarios',str),('ubicacion',coordenadas)])
 
 def lee_avistamientos(ruta:str)->List[avistamiento]:
     lista=list()
@@ -56,3 +56,73 @@ def duracion_total(av:List[avistamiento],estado:str)->int:
 def duracion_total2(av:List[avistamiento],estado:str)->int:
     suma=list(a.duracion for a in av if a.estado==estado)
     return sum(suma)
+
+def conversion_radianes(coord:coordenadas)->coordenadas:
+    latitud=coord[0]*pi/180
+    longitud=coord[1]*pi/180
+    return coordenadas(latitud,longitud)
+
+def calcular_distancia(coord1:coordenadas,coord2:coordenadas)->float:
+    coord1=conversion_radianes(coord1)
+    coord2=conversion_radianes(coord2)
+    inc_lat=coord2.latitud-coord1.latitud
+    inc_long=coord2.longitud-coord1.longitud
+    a=(sin(inc_lat/2)**2) + (cos(coord1.latitud)*cos(coord2.latitud)*(sin(inc_long/2)**2))
+    distancia=2*6371*asin(sqrt(a))
+    return distancia
+
+def avistamientos_cercanos_ubicacion(av:List[avistamiento],ubicacion:coordenadas,radio:float)->Set[avistamiento]:
+    conjunto=set()
+    for a in av:
+        distancia=calcular_distancia(ubicacion,a.ubicacion)
+        if distancia<=radio:
+            conjunto.add(a)
+    return conjunto
+
+def avistamientos_cercanos_ubicacion2(av:List[avistamiento],ubicacion:coordenadas,radio:float)->Set[avistamiento]:
+    return set(a for a in av if radio>=calcular_distancia(ubicacion,a.ubicacion))
+
+def avistamiento_mayor_duracion(av:List[avistamiento],forma:str)->avistamiento:
+    lista=list()
+    for a in av:
+        if a.forma==forma:
+            lista.append(a)
+    return max(lista, key=lambda e:e.duracion)
+
+def avistamiento_mayor_duracion2(av:List[avistamiento],forma:str)->avistamiento:
+    return max(list(a for a in av if a.forma==forma), key=lambda e:e.duracion)
+
+def avistamiento_cercano_mayor_duracion(av:List[avistamiento],radio:float,ubicacion:coordenadas)->Tuple[int,str]:
+    conj=avistamientos_cercanos_ubicacion2(av,ubicacion,radio)
+    maximo=max(conj,key=lambda e:e.duracion)
+    return (maximo.duracion,maximo.comentarios)
+
+def avistamiento_cercano_mayor_duracion2(av:List[avistamiento],radio:float,ubicacion:coordenadas)->Tuple[int,str]:
+    maximo=max(avistamientos_cercanos_ubicacion2(av,ubicacion,radio),key=lambda e:e.duracion)
+    return (maximo.duracion,maximo.comentarios)
+
+def avistamiento_fechas(av:List[avistamiento],fecha1:Optional[date]=None,fecha2:Optional[date]=None)->List[avistamiento]:
+    return sorted(list(a for a in av if (fecha1==None or a.fecha_hora.date()>=fecha1) and 
+                       (fecha2==None or a.fecha_hora.date()<=fecha2)),key=lambda e:e.fecha_hora, reverse=True)
+
+def comentario_mas_largo(av:List[avistamiento],palabra:str,año:int)->avistamiento:
+    lista=list()
+    for a in av:
+        if (palabra in a.comentarios) and (a.fecha_hora.year==año):
+            lista.append(a)
+    return max(lista, key=lambda e:len(e.comentarios))
+
+def comentario_mas_largo2(av:List[avistamiento],palabra:str,año:int)->avistamiento:
+    return max(list(a for a in av if palabra in a.comentarios and a.fecha_hora.year==año),key=lambda e:len(e.comentarios))
+
+def media_dias_entre_avistamientos(av:List[avistamiento],año:Optional[int]=None)->float:
+    lista=list()
+    dias=list()
+    for a in av:
+        if año==None or a.fecha_hora.year==año:
+            lista.append(a)
+    ordenada=sorted(lista, key=lambda e:e.fecha_hora)
+    for d1,d2 in zip(ordenada[0:],ordenada[1:]):
+        diferencia=(d2.fecha_hora.date()-d1.fecha_hora.date()).days
+        dias.append(diferencia)
+    return sum(dias)/len(dias)
--- src_practica/test_avistamientos2.py
+++ src_practica/test_avistamientos2.py
@@ -17,12 +17,58 @@ def test_duracion_total(av:List[avistamiento],estado:str)->None:
     duracion=duracion_total(av,estado)
     print(f"Duración total de los avistamientos en {estado}: {duracion} segundos.")
 
+def test_avistamientos_cercanos_ubicacion(av:List[avistamiento],ubicacion:coordenadas)->None:
+    radio=0.5
+    conjunto=avistamientos_cercanos_ubicacion(av,ubicacion,radio)
+    print(f"Avistamientos cercanos a {ubicacion}:")
+    for num,conj in enumerate(conjunto):
+        print(f"{num+1}. {conj}")
+
+def test_avistamiento_mayor_duracion(av:List[avistamiento],forma:str)->None:
+    final=avistamiento_mayor_duracion2(av,forma)
+    print(f"Avistamiento de forma {forma} de mayor duracion: {final}")
+
+def test_avistamiento_cercano_mayor_duracion(av:List[avistamiento],ubicacion:coordenadas)->None:
+    radio=0.5
+    final=avistamiento_cercano_mayor_duracion2(av,radio,ubicacion)
+    print(f"Duración del avistamiento más largo en un entorno de radio {radio} sobre las coordenadas {ubicacion}: {final[0]}")
+    print(f"Comentario: {final[1]}")
+
+def test_avistamiento_fechas(av:List[avistamiento],fecha1:date,fecha2:date)->None:
+    meses=['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+    dia1=fecha1.day
+    dia2=fecha2.day
+    mes1=meses[fecha1.month-1]
+    mes2=meses[fecha2.month-1]
+    año1=fecha1.year
+    año2=fecha2.year
+    print(f"Mostrando los avistamientos desde el {dia1} de {mes1} del {año1} hasta el {dia2} de {mes2} del {año2}:")
+    for p in avistamiento_fechas(av,fecha1,fecha2):
+        print(p)
+    print(f"Total: {len(avistamiento_fechas(av,fecha1,fecha2))} avistamientos.")
+    print(f"Avistmaientos hasta el {dia1} de {mes1} del {año1}: {len(avistamiento_fechas(av,fecha2=fecha1))}")
+    print(f"Avistamientos desde el {dia2} de {mes2} del {año2}: {len(avistamiento_fechas(av,fecha2))}")
+
+def test_comentario_mas_largo(av:List[avistamiento],palabra:str,año:int)->None:
+    print(f"El avistamiento con el comentario más largo del {año} incluyendo la palabra \"{palabra}\" es:")
+    print(comentario_mas_largo2(av,palabra,año))
+
+def test_media_dias_entre_avistamientos(av:List[avistamiento],año:int)->None:
+    print(f"La media de dias entre dos avistamientos consecutivos es: {media_dias_entre_avistamientos(av)}")
+    print(f"La media de dias entre dos avistamientos consecutivos del año {año} es: {media_dias_entre_avistamientos(av,año)}")
+
 if __name__=='__main__':
     ovni=lee_avistamientos('data/ovnis.csv')
     #test_lee_avistamiento(ovni)
-    #test_numero_avistamientos_fecha(ovni,datetime(2005,5,1).date())
+    #test_numero_avistamientos_fecha(ovni,date(2005,5,1))
     #test_formas_estado(ovni,{'nm','in','pa','wa'})
     #test_duracion_total(ovni,'in')
     #test_duracion_total(ovni,'nm')
     #test_duracion_total(ovni,'pa')
     #test_duracion_total(ovni,'wa')
+    #test_avistamientos_cercanos_ubicacion(ovni,coordenadas(40.1933333, -85.3863889))
+    #test_avistamiento_mayor_duracion(ovni,'circle')
+    #test_avistamiento_cercano_mayor_duracion(ovni,coordenadas(40.1933333, -85.3863889))
+    #test_avistamiento_fechas(ovni,date(2005,5,1),date(2005,5,1))
+    #test_comentario_mas_largo(ovni,'ufo',2005)
+    #test_media_dias_entre_avistamientos(ovni,1979)
